37c80b4e4da82d1172053b0ea81b30acc5597fd1
github username <github email address>
2021-09-04 12:39:54 -0600

    completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
-
+class Student < User
+    def initialize
+        @knowledge = []
+    end
+    def learn(str = Teacher.new)
+        @knowledge.push(str)
+    end
+    def knowledge
+        @knowledge
+    end
 end
